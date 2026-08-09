0195b9ed421e414dc92440069020300521d8f681
Vikral Yadav <123933280+vikralyadav@users.noreply.github.com>
2026-08-09 21:27:24 +0530

    Add: kth-largest-element-in-an-array - submission-2

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+          int n = nums.size();
+
+
+        sort(nums.begin(), nums.end());
+
+
+       return  nums[n-k];
+        
+    }
+};
