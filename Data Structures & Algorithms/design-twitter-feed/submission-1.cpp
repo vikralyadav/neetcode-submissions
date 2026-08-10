16321e6d18f1a2c4346db916371883154ca7648d
Vikral Yadav <123933280+vikralyadav@users.noreply.github.com>
2026-08-10 20:30:32 +0530

    Add: design-twitter-feed - submission-1

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-1.cpp
@@ -0,0 +1,87 @@
+class Twitter {
+private:
+    int time;
+
+    unordered_map<int, vector<pair<int, int>>> tweets;
+
+    unordered_map<int, unordered_set<int>> following;
+
+public:
+
+    Twitter() {
+        time = 0;
+    }
+
+    void postTweet(int userId, int tweetId) {
+        tweets[userId].push_back({time++, tweetId});
+    }
+
+    vector<int> getNewsFeed(int userId) {
+
+       
+        priority_queue<
+            tuple<int, int, int>
+        > pq;
+
+
+        if (!tweets[userId].empty()) {
+            int index = tweets[userId].size() - 1;
+
+            pq.push({
+                tweets[userId][index].first,
+                userId,
+                index
+            });
+        }
+
+     
+        for (int followee : following[userId]) {
+
+            if (!tweets[followee].empty()) {
+
+                int index = tweets[followee].size() - 1;
+
+                pq.push({
+                    tweets[followee][index].first,
+                    followee,
+                    index
+                });
+            }
+        }
+
+        vector<int> ans;
+
+        // Get 10 most recent tweets
+        while (!pq.empty() && ans.size() < 10) {
+
+            auto [timestamp, user, index] = pq.top();
+            pq.pop();
+
+            ans.push_back(tweets[user][index].second);
+
+            // Move to the next older tweet of this user
+            if (index > 0) {
+
+                int nextIndex = index - 1;
+
+                pq.push({
+                    tweets[user][nextIndex].first,
+                    user,
+                    nextIndex
+                });
+            }
+        }
+
+        return ans;
+    }
+
+   void follow(int followerId, int followeeId) {
+    if (followerId != followeeId) {
+        following[followerId].insert(followeeId);
+    }
+}
+
+    void unfollow(int followerId, int followeeId) {
+        following[followerId].erase(followeeId);
+    }
+};
