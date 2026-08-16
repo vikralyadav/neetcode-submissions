d36966b66fa1447215e2fcba4bf8ee0b7373c749
Vikral Yadav <123933280+vikralyadav@users.noreply.github.com>
2026-08-16 21:26:36 +0530

    Add: sliding-window-maximum - submission-3

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+
+
+
+       vector<int> ans;
+
+    for (int i = 0; i <= nums.size() - k; i++) {
+
+        int maxi = INT_MIN;
+
+        for (int j = i; j < i + k; j++) {
+            maxi = max(maxi, nums[j]);
+        }
+
+        ans.push_back(maxi);
+    }
+
+    return ans;
+    }
+};
